f1a3b79e88a8db4df3ce914ef692ed3bd78e1848
DannyMcwaves <dannymcwaves96@rocketmail.com>
2016-12-13 12:27:47 +0000

    adding a reference link

---
+++ docs/array.js
@@ -0,0 +1,6 @@
+
+// the syntax if an array remians farly the same.
+// there are just  some new methods that are now added to the array.
+// be sure to check it out at MDN.
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+//
